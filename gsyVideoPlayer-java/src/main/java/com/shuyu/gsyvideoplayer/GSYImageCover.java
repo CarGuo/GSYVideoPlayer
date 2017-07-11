64379cec027439f7a16716a0910d7014b309aa21
shuyu <359369982@qq.com>
2017-07-11 21:50:02 +0800

    修改了布局现实的问题 (2017-07-11)

--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/GSYImageCover.java
+++ gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/GSYImageCover.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.shuyu.gsyvideoplayer.utils.GSYVideoType;
+import com.shuyu.gsyvideoplayer.utils.MeasureHelper;
 
 /**
  * Created by shuyu on 2016/12/6.
@@ -14,21 +15,25 @@ import com.shuyu.gsyvideoplayer.utils.GSYVideoType;
 @SuppressLint("AppCompatCustomView")
 public class GSYImageCover extends ImageView {
 
-
-    private boolean fullView;
-
-    private int originW, originH;
+    private MeasureHelper measureHelper;
 
     public GSYImageCover(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public GSYImageCover(Context context) {
         super(context);
+        init();
     }
 
     public GSYImageCover(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        measureHelper = new MeasureHelper(this);
     }
 
 
@@ -37,176 +42,13 @@ public class GSYImageCover extends ImageView {
 
         int videoWidth = GSYVideoManager.instance().getCurrentVideoWidth();
         int videoHeight = GSYVideoManager.instance().getCurrentVideoHeight();
-
-        int width = getDefaultSize(videoWidth, widthMeasureSpec);
-        int height = getDefaultSize(videoHeight, heightMeasureSpec);
-
-        int widthS = getDefaultSize(videoWidth, widthMeasureSpec);
-        int heightS = getDefaultSize(videoHeight, heightMeasureSpec);
-
-        if (GSYVideoType.getShowType() == GSYVideoType.SCREEN_MATCH_FULL) {
-            boolean rotate = (getRotation() != 0 && getRotation() % 90 == 0 && Math.abs(getRotation()) != 180);
-            if(rotate) {
-                setMeasuredDimension(heightMeasureSpec, widthMeasureSpec);
-            } else {
-                setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
-            }
-            return;
-        }
-
-
-
-        if (originW == 0 || originH == 0) {
-            originW = widthS;
-            originH = heightS;
-        }
-
         if (videoWidth > 0 && videoHeight > 0) {
-
-            int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-            int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
-            int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
-            int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
-
-            if (widthSpecMode == MeasureSpec.EXACTLY && heightSpecMode == MeasureSpec.EXACTLY) {
-                width = widthSpecSize;
-                height = heightSpecSize;
-
-                if (videoWidth * height < width * videoHeight) {
-                    width = height * videoWidth / videoHeight;
-                } else if (videoWidth * height > width * videoHeight) {
-                    height = width * videoHeight / videoWidth;
-                }
-            } else if (widthSpecMode == MeasureSpec.EXACTLY) {
-                width = widthSpecSize;
-                height = width * videoHeight / videoWidth;
-                if (heightSpecMode == MeasureSpec.AT_MOST && height > heightSpecSize) {
-                    height = heightSpecSize;
-                }
-            } else if (heightSpecMode == MeasureSpec.EXACTLY) {
-                height = heightSpecSize;
-                width = height * videoWidth / videoHeight;
-                if (widthSpecMode == MeasureSpec.AT_MOST && width > widthSpecSize) {
-                    width = widthSpecSize;
-                }
-            } else {
-                width = videoWidth;
-                height = videoHeight;
-                if (heightSpecMode == MeasureSpec.AT_MOST && height > heightSpecSize) {
-                    height = heightSpecSize;
-                    width = height * videoWidth / videoHeight;
-                }
-                if (widthSpecMode == MeasureSpec.AT_MOST && width > widthSpecSize) {
-                    width = widthSpecSize;
-                    height = width * videoHeight / videoWidth;
-                }
-            }
-        } else {
-            // no size yet, just adopt the given spec sizes
+            measureHelper.setVideoSize(videoWidth, videoHeight);
         }
+        measureHelper.setVideoRotation((int) getRotation());
+        measureHelper.doMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        boolean rotate = (getRotation() != 0 && getRotation() % 90 == 0 && Math.abs(getRotation()) != 180);
-        if (rotate) {
-            if (widthS < heightS) {
-                if (width > height) {
-                    width = (int) (width * (float) widthS / height);
-                    height = widthS;
-                } else {
-                    height = (int) (height * (float) width / widthS);
-                    width = widthS;
-                }
-            } else {
-                if (width > height) {
-                    height = (int) (height * (float) width / widthS);
-                    width = widthS;
-                } else {
-                    width = (int) (width * (float) widthS / height);
-                    height = widthS;
-                }
-            }
-
-            //如果旋转后的高度大于宽度
-            if (width > height) {
-                //如果视频的旋转后，width（高度）大于控件高度，需要压缩下高度
-                if (originH < originW) {
-                    if (width > heightS) {
-                        height = (int) (height * ((float) width / heightS));
-                        width = heightS;
-                    }
-                } else {
-                    if (width > heightS) {
-                        height = (int) (height / ((float) width / heightS));
-                        width = heightS;
-                    }
-                }
-            } else {
-                //如果旋转后的宽度大于高度
-                if (height > widthS) {
-                    width = (int) (width * ((float) height / widthS));
-                    height = widthS;
-                }
-            }
-        }
-
-        //如果设置了比例
-        if (GSYVideoType.getShowType() == GSYVideoType.SCREEN_TYPE_16_9) {
-            if (height > width) {
-                width = height * 9 / 16;
-            } else {
-                height = width * 9 / 16;
-            }
-        } else if (GSYVideoType.getShowType() == GSYVideoType.SCREEN_TYPE_4_3) {
-            if (height > width) {
-                width = height * 3 / 4;
-            } else {
-                height = width * 3 / 4;
-            }
-        }
-
-        fullView = (GSYVideoType.getShowType() == GSYVideoType.SCREEN_TYPE_FULL);
-
-        //上面会调整一变全屏，这里如果要全屏裁减，就整另外一边
-        if (fullView) {
-            if (rotate && getRotation() != 0) {
-                if (width > height) {
-                    if (height < originW) {
-                        width = (int) (width * ((float) originW / height));
-                        height = originW;
-                    } else if (width < originH) {
-                        height = (int) (height * ((float) originH / width));
-                        width = originH;
-                    }
-                } else {
-                    if (width < originH) {
-                        height = (int) (height * ((float) originH / width));
-                        width = originH;
-                    } else if (height < originW) {
-                        width = (int) (width * ((float) originW / height));
-                        height = originW;
-                    }
-                }
-            } else {
-                if (height > width) {
-                    if (width < widthS) {
-                        height = (int) (height * ((float) widthS / width));
-                        width = widthS;
-                    } else {
-                        width = (int) (width * ((float) heightS / height));
-                        height = heightS;
-                    }
-                } else {
-                    if (height < heightS) {
-                        width = (int) (width * ((float) heightS / height));
-                        height = heightS;
-                    } else {
-                        height = (int) (height * ((float) widthS / width));
-                        width = widthS;
-                    }
-                }
-            }
-        }
-
-        setMeasuredDimension(width, height);
+        setMeasuredDimension(measureHelper.getMeasuredWidth(), measureHelper.getMeasuredHeight());
     }
 
 }
