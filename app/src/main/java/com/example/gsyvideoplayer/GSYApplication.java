2e67ebee68d94e3b688dda7bb73e4a52738281a4
shuyu <359369982@qq.com>
2017-09-12 22:24:37 +0800

    增加硬解码优化播放，虽然貌似没用,for fun (2017-09-12）

--- app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
+++ app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -23,6 +23,7 @@ public class GSYApplication extends Application {
         //}
         //LeakCanary.install(this);
         //GSYVideoType.enableMediaCodec();
+        //GSYVideoType.enableMediaCodecTexture();
         //GSYVideoManager.instance().setVideoType(this, GSYVideoType.IJKEXOPLAYER);
         //GSYVideoType.setShowType(GSYVideoType.SCREEN_MATCH_FULL);
         //GSYVideoType.setShowType(GSYVideoType.SCREEN_TYPE_FULL);
