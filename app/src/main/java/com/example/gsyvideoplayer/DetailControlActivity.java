4f16ee8c9eb9c9b4ee30a2998fc0fcb1748a55b9
guoshuyu <guoshuyu@ygsoft.com>
2017-10-31 10:11:04 +0800

    增加竖屏全屏demo (2017-10-31)

--- app/src/main/java/com/example/gsyvideoplayer/DetailControlActivity.java
+++ app/src/main/java/com/example/gsyvideoplayer/DetailControlActivity.java
@@ -1,5 +1,6 @@
 package com.example.gsyvideoplayer;
 
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.widget.NestedScrollView;
@@ -89,10 +90,10 @@ public class DetailControlActivity extends GSYBaseActivityDetail {
         detailPlayer.setLockClickListener(new LockClickListener() {
             @Override
             public void onClick(View view, boolean lock) {
-                if (orientationUtils != null) {
-                    //配合下方的onConfigurationChanged
-                    orientationUtils.setEnable(!lock);
-                }
+                //if (orientationUtils != null) {
+                //配合下方的onConfigurationChanged
+                //orientationUtils.setEnable(!lock);
+                //}
             }
         });
 
@@ -160,7 +161,7 @@ public class DetailControlActivity extends GSYBaseActivityDetail {
                 .setIsTouchWiget(true)
                 .setRotateViewAuto(false)
                 .setLockLand(false)
-                .setShowFullAnimation(false)
+                .setShowFullAnimation(true)//打开动画
                 .setNeedLockFull(true)
                 .setSeekRatio(1);
     }
@@ -176,14 +177,49 @@ public class DetailControlActivity extends GSYBaseActivityDetail {
         mGifCreateHelper.cancelTask();
     }
 
+
+    /*******************************竖屏全屏开始************************************/
+
+    @Override
+    public void initVideo() {
+        super.initVideo();
+        //重载后实现点击，不横屏
+        if (getGSYVideoPlayer().getFullscreenButton() != null) {
+            getGSYVideoPlayer().getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+                    getGSYVideoPlayer().startWindowFullscreen(DetailControlActivity.this, true, true);
+                }
+            });
+        }
+    }
+
+
     /**
      * 是否启动旋转横屏，true表示启动
+     *
      * @return true
      */
     @Override
     public boolean getDetailOrientationRotateAuto() {
-        return true;
+        return false;
     }
+
+    //重载后关闭重力旋转
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        orientationUtils.setEnable(false);
+    }
+
+    //重载后不做任何事情，实现竖屏全屏
+    @Override
+    public void onQuitFullscreen(String url, Object... objects) {
+        super.onQuitFullscreen(url, objects);
+    }
+
+    /*******************************竖屏全屏结束************************************/
 
     private void initGifHelper() {
         mGifCreateHelper = new GifCreateHelper(detailPlayer, new GSYVideoGifSaveListener() {
