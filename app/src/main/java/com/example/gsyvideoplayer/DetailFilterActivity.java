ec355f59e9134b168ab93e4783a36918b06b75b7
guoshuyu <guoshuyu@ygsoft.com>
2018-04-16 13:44:25 +0800

    gif permission tip

--- app/src/main/java/com/example/gsyvideoplayer/DetailFilterActivity.java
+++ app/src/main/java/com/example/gsyvideoplayer/DetailFilterActivity.java
@@ -347,7 +347,7 @@ public class DetailFilterActivity extends GSYBaseActivityDetail<StandardGSYVideo
      * 开始gif截图
      */
     private void startGif() {
-        Toast.makeText(this, "项目里目前没做动态授权，需要你去做设置里授权哦", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "项目里目前没做动态授权，需要你去做设置里授权文件读取哦", Toast.LENGTH_LONG).show();
         //开始缓存各个帧
         mGifCreateHelper.startGif(new File(FileUtils.getPath()));
 
@@ -357,7 +357,7 @@ public class DetailFilterActivity extends GSYBaseActivityDetail<StandardGSYVideo
      * 生成gif
      */
     private void stopGif() {
-        Toast.makeText(this, "项目里目前没做动态授权，需要你去做设置里授权哦", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "项目里目前没做动态授权，需要你去做设置里授权文件读取哦", Toast.LENGTH_LONG).show();
         loadingView.setVisibility(View.VISIBLE);
         mGifCreateHelper.stopGif(new File(FileUtils.getPath(), "GSY-Z-" + System.currentTimeMillis() + ".gif"));
     }
