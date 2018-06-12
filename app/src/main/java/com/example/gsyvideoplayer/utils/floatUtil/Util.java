c071ef47071e397638df11e955c572ab6c6d28df
haohao <358297604@qq.com>
2018-06-12 15:00:04 +0800

    修复8.0+悬浮窗权限

--- app/src/main/java/com/example/gsyvideoplayer/utils/floatUtil/Util.java
+++ app/src/main/java/com/example/gsyvideoplayer/utils/floatUtil/Util.java
@@ -2,7 +2,9 @@ package com.example.gsyvideoplayer.utils.floatUtil;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -22,16 +24,9 @@ public class Util {
         return inflate.inflate(layoutId, null);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public static boolean hasPermission(Context context) {
-        Boolean result;
-        try {
-            Class clazz = Settings.class;
-            Method canDrawOverlays = clazz.getDeclaredMethod("canDrawOverlays", Context.class);
-            result = (Boolean) canDrawOverlays.invoke(null, context);
-        } catch (Exception e) {
-            result = false;
-        }
-        return result;
+        return Settings.canDrawOverlays(context);
     }
 
 
