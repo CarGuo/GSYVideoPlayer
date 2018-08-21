7d78f652c000fff34ef4c94857da51b0d35802a0
guoshuyu <guoshuyu@ygsoft.com>
2018-08-21 15:06:41 +0800

    fix

--- app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
+++ app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -35,11 +35,11 @@ public class GSYApplication extends MultiDexApplication {
         //GSYVideoType.enableMediaCodec();
         //GSYVideoType.enableMediaCodecTexture();
 
-        PlayerFactory.setPlayManager(new Exo2PlayerManager());//EXO模式
+        //PlayerFactory.setPlayManager(new Exo2PlayerManager());//EXO模式
         //PlayerFactory.setPlayManager(new SystemPlayerManager());//系统模式
         //PlayerFactory.setPlayManager(new IjkPlayerManager());//ijk模式
 
-        CacheFactory.setCacheManager(new ExoPlayerCacheManager());//exo缓存模式，支持m3u8，只支持exo
+        //CacheFactory.setCacheManager(new ExoPlayerCacheManager());//exo缓存模式，支持m3u8，只支持exo
         //CacheFactory.setCacheManager(new ProxyCacheManager());//代理缓存模式，支持所有模式，不支持m3u8等
 
         //GSYVideoType.setShowType(GSYVideoType.SCREEN_MATCH_FULL);
