cd5bbea5df50720956228fe7af1ff42059ebb826
guoshuyu <guoshuyu@ygsoft.com>
2018-09-10 14:20:32 +0800

    change demo mode

--- app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
+++ app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -39,11 +39,11 @@ public class GSYApplication extends MultiDexApplication {
         //GSYVideoType.enableMediaCodec();
         //GSYVideoType.enableMediaCodecTexture();
 
-        PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
+        //PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
         //PlayerFactory.setPlayManager(SystemPlayerManager.class);//系统模式
         //PlayerFactory.setPlayManager(IjkPlayerManager.class);//ijk模式
 
-        CacheFactory.setCacheManager(ExoPlayerCacheManager.class);//exo缓存模式，支持m3u8，只支持exo
+        //CacheFactory.setCacheManager(ExoPlayerCacheManager.class);//exo缓存模式，支持m3u8，只支持exo
         //CacheFactory.setCacheManager(ProxyCacheManager.class);//代理缓存模式，支持所有模式，不支持m3u8等
 
         //GSYVideoType.setShowType(GSYVideoType.SCREEN_MATCH_FULL);
--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/render/view/RenderFactory.java
+++
@@ -1,8 +0,0 @@
-package com.shuyu.gsyvideoplayer.render.view;
-
-/**
- * Created by guoshuyu
- * Date: 2018-08-30
- */
-public class RenderFactory {
-}
