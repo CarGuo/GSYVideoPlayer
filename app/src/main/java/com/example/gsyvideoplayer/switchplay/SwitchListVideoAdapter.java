47c6d49748c73b46031bc54c6ad64c58437e6fb0
guoshuyu <359369982@qq.com>
2019-07-29 11:39:27 +0800

    update demo

--- app/src/main/java/com/example/gsyvideoplayer/switchplay/SwitchListVideoAdapter.java
+++ app/src/main/java/com/example/gsyvideoplayer/switchplay/SwitchListVideoAdapter.java
@@ -70,7 +70,7 @@ public class SwitchListVideoAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
         final String urlH = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
-        final String urlV = "http://video.7k.cn/app_video/20171202/6c8cf3ea/v.m3u8.mp4";
+        final String urlV = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4";
         final String url = (position % 2 == 0) ? urlH : urlV;
         final int coverId = (position % 2 == 0) ? R.mipmap.xxx1 : R.mipmap.xxx2;
 
