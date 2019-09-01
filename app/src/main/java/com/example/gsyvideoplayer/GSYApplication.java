7bbd38f4e3c92ab1ac7f6da22b24bd69e85feb36
shuyu <359369982@qq.com>
2019-09-01 22:55:17 +0800

    update demo

--- app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
+++ app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -37,7 +37,7 @@ public class GSYApplication extends MultiDexApplication {
         //GSYVideoType.enableMediaCodec();
         //GSYVideoType.enableMediaCodecTexture();
 
-        PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
+        //PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
         //ExoSourceManager.setSkipSSLChain(true);
 
 
