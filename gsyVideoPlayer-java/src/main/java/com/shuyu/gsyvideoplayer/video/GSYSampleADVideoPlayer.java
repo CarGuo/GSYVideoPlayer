629d6da3f8817dfc7d5358b5fa29b1f72f855641
guoshuyu <359369982@qq.com>
2019-11-19 11:47:26 +0800

    #2411

--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/video/GSYSampleADVideoPlayer.java
+++ gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/video/GSYSampleADVideoPlayer.java
@@ -223,8 +223,8 @@ public class GSYSampleADVideoPlayer extends ListGSYVideoPlayer {
     }
 
     @Override
-    protected void setProgressAndTime(int progress, int secProgress, int currentTime, int totalTime) {
-        super.setProgressAndTime(progress, secProgress, currentTime, totalTime);
+    protected void setProgressAndTime(int progress, int secProgress, int currentTime, int totalTime, boolean forceChange) {
+        super.setProgressAndTime(progress, secProgress, currentTime, totalTime, forceChange);
         if (mADTime != null && currentTime > 0) {
             int totalSeconds = totalTime / 1000;
             int currentSeconds = currentTime / 1000;
