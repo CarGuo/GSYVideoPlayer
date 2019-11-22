6fd6762bbd1d079532e3d877af44c090daa9b2d9
guoshuyu <359369982@qq.com>
2019-11-22 09:58:36 +0800

    适配 android 10 的全屏

--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/CommonUtil.java
+++ gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/CommonUtil.java
@@ -163,7 +163,15 @@ public class CommonUtil {
     }
 
     public static void hideNavKey(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= 29) {
+            //       设置屏幕始终在前面，不然点击鼠标，重新出现虚拟按键
+            ((Activity) context).getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav
+                            // bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             //       设置屏幕始终在前面，不然点击鼠标，重新出现虚拟按键
             ((Activity) context).getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
