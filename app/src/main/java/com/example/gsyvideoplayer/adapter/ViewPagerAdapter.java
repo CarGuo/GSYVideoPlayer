639e15f1a1cebeb53e093b34619ca4bd5dc6b032
guoshuyu <359369982@qq.com>
2020-10-16 10:22:54 +0800

    add viewpager2

---
+++ app/src/main/java/com/example/gsyvideoplayer/adapter/ViewPagerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.gsyvideoplayer.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gsyvideoplayer.R;
+import com.example.gsyvideoplayer.holder.RecyclerItemNormalHolder;
+import com.example.gsyvideoplayer.model.VideoModel;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final static String TAG = "RecyclerBaseAdapter";
+
+    private List<VideoModel> itemDataList = null;
+    private Context context = null;
+
+    public ViewPagerAdapter(Context context, List<VideoModel> itemDataList) {
+        this.itemDataList = itemDataList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                      int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.layout_viewpager2_item, parent, false);
+        final RecyclerView.ViewHolder holder = new RecyclerItemNormalHolder(context, v);
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        RecyclerItemNormalHolder recyclerItemViewHolder = (RecyclerItemNormalHolder) holder;
+        recyclerItemViewHolder.setRecyclerBaseAdapter(this);
+        recyclerItemViewHolder.onBind(position, itemDataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemDataList.size();
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return 1;
+    }
+
+    public void setListData(List<VideoModel> data) {
+        itemDataList = data;
+        notifyDataSetChanged();
+    }
+}
