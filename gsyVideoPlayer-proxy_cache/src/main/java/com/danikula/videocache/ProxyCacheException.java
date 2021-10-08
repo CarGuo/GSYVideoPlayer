9696909ff225063cbc4b03af01239f07f7efbbe2
Goooler <wangzongler@gmail.com>
2021-10-08 17:14:21 +0800

    Adopt AGP 7.0 (#3349)
    * Update dependencies
    
    (cherry picked from commit a2bb7804059fee5aead9f0af2e0917ea549a3b01)
    
    * Adapt AGP 7.0
    
    * Replace http with https
    
    * Replace rootDir use
    
    * Add .editorconfig rule
    
    * Move versions into base.gradle
    
    * Replace inverted commas
    
    * Add lib.gradle
    
    * Rename sign key
    
    * Add test dependencies & consumerProguardFiles
    
    * Bump github checkout action to v2
    
    * Bump gradle to 7.2
    
    * Adapt ExoPlayer 2.15.0
    
    * Bump AGP to 7.0.2
    
    * Revert "Replace http with https"
    
    This reverts commit 3e6c5e33
    
    * Revert "Bump github checkout action to v2"
    
    This reverts commit 86fb0334
    
    * Revert "Add .editorconfig rule"
    
    This reverts commit d9aa5428
    
    * Revert "Adapt ExoPlayer 2.15.0"
    
    This reverts commit ffd36a69
    
    * Remove extra line
    
    * Replace unused lines
    
    * Move dependencies.gradle into gradle folder
    
    * Bump exo to 2.15.1
    
    * Disable BuildConfig by default.
    
    * Inject LIBRARY_VERSION into BuildConfig
    
    * Add gradle cache actions
    
    * Simplify ci.yml
    
    * Bump okhttp to 4.9.2

--- gsyVideoPlayer-proxy_cache/src/main/java/com/danikula/videocache/ProxyCacheException.java
+++ gsyVideoPlayer-proxy_cache/src/main/java/com/danikula/videocache/ProxyCacheException.java
@@ -9,17 +9,15 @@ import shuyu.com.androidvideocache.BuildConfig;
  */
 public class ProxyCacheException extends Exception {
 
-    private static final String LIBRARY_VERSION = ". Version: " + BuildConfig.VERSION_NAME;
-
     public ProxyCacheException(String message) {
-        super(message + LIBRARY_VERSION);
+        super(message + BuildConfig.LIBRARY_VERSION);
     }
 
     public ProxyCacheException(String message, Throwable cause) {
-        super(message + LIBRARY_VERSION, cause);
+        super(message + BuildConfig.LIBRARY_VERSION, cause);
     }
 
     public ProxyCacheException(Throwable cause) {
-        super("No explanation error" + LIBRARY_VERSION, cause);
+        super("No explanation error" + BuildConfig.LIBRARY_VERSION, cause);
     }
 }
